25da1620f6bee8949a5b29d9feaaf630cc95ad23
Rye <git@itsrye.dev>
2025-11-16 20:04:03 +0100

    meow

--- src/components/LovedTracks.vue
+++ src/components/LovedTracks.vue
@@ -24,9 +24,9 @@ watchEffect(async () => {
 
 <template>
      <ul v-if="lovedTracks">
-      <li v-for="track in lovedTracks.getLovedTracks" :key="track.name">
+      <!--li v-for="track in lovedTracks.getLovedTracks" :key="track.name">
         {{ track.name }} – {{ track.artist }}
-      </li>
+      </li-->
     </ul>
     <div class="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
     </div>
