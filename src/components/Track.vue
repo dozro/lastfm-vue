c4e06902302cdfe28f05f057037845db1443eca2
Rye <git@itsrye.dev>
2025-11-17 12:23:17 +0100

    added Track.vue

---
+++ src/components/Track.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const props = defineProps<{
+  title: string,
+  artist: string,
+  url: string,
+}>()
+</script>
+
+<template>
+    <div class="track-container">
+
+    </div>
+</template>
+
+<style scoped lang="scss">
+@import "tailwindcss";
+.track-container{
+    border:1pt
+}
+</style>
